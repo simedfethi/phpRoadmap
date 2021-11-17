340592324416b36c31049d17cd6f1e54c604d4c4
FETHI SIMED <simedfethi@gmail.com>
2021-11-17 13:06:10 +0100

    exressions

---
+++ expressions.php
@@ -0,0 +1,31 @@
+<?php
+3 . 2.74 ;// gives the string 32.74
+"9 Lives" - 1; // 8 (int)
+"3.14 Pies" * 2; // 6.28 (float)
+"9. Lives" - 1; // 8 (float / double)
+"1E3 Points of Light" + 1; // 1001 (float)
+
+// exponentiation
+$var1 = 5;
+$var2 = 2;
+echo $var1 ** $var2; // outputs 125
+
+$n = 5;
+$s = 'There were ' . $n . ' ducks.';
+// $s is 'There were 5 ducks'
+
+$var1 = 5;
+$var2 = 65;
+echo $var1 <=> $var2 ; // outputs -1
+echo $var2 <=> $var1 ; // outputs 1
+
+$var1 = null;
+$var2 = 31;
+echo $var1 ?? $var2 ; //outputs 31
+printf("\r\n");
+
+printf(decbin(1028866). "\r\n");
+
+
+
+$x=1;

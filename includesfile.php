1a50f6b06e89edbbdcb53f94fa10babc9a05f6d3
FETHI SIMED <simedfethi@gmail.com>
2021-11-17 15:28:59 +0100

    includes

--- includesfile.php
+++ includesfile.php
@@ -2,3 +2,23 @@
 <?php include "base.html"; ?>
     content
 <?php include "footer.html"; ?>
+<?php
+include "codelib.php";
+//require "codelib.php"; // require block execution if not found , include is more forgiving.
+//include "http://www.example.com/codelib.php";
+
+for ($i = 0; $i < 10; $i++) {
+    include_once "base.html";
+   // include "base.html";
+}
+$list= get_included_files();
+foreach ($list as $s)
+{
+    printf($s. "\r\n");
+
+
+}
+$x=1;
+?>
+<? echo "Hello, world"; ?>
+<input type="text" name="first_name" value="<?= "Dawn"; ?>">

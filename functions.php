e2f5295de3c51f664825af583951d2d19bfada06
FETHI SIMED <simedfethi@gmail.com>
2021-11-17 17:05:26 +0100

    functions

--- functions.php
+++ functions.php
@@ -77,5 +77,104 @@ function getPreferences($whichPreference = 'all')
 // otherwise, get the specific preference requested...
 }
 
+function countList()
+{
+    if (func_num_args() == 0) {
+        return false;
+    }
+    else {
+        $count = 0;
+        for ($i = 0; $i < func_num_args(); $i++) {
+            $count += func_get_arg($i);
+        }
+        return $count;
+    }
+}
+echo countList(1, 5, 9); // outputs "15"
+
+
+
+function takesTwo($a, $b)
+{
+    if (isset($a)) {
+        echo " a is set\n";
+    }
+    if (isset($b)) {
+        echo " b is set\n";
+    }
+}
+echo "With two arguments:\n";
+takesTwo(1, 2);
+echo "With one argument:\n";
+takesTwo(1,3);
+
+
+
+class Entertainment {}
+class Clown extends Entertainment {}
+class Job {}
+function handleEntertainment(Entertainment $a, callable $callback = NULL)
+{
+    echo "Handling " . get_class($a) . " fun\n";
+    if ($callback !== NULL) {
+        $callback();
+    }
+}
+$callback = function()
+{
+// do something
+};
+handleEntertainment(new Clown); // works
+handleEntertainment(new Clown, $callback); // runtime error
+
+function returnOne()
+{
+    return 42;
+}
+function returnArray()
+{
+    return array("ddd","vfgf");
+
+}
+
+function someMath($var1, $var2): int
+{
+    return $var1 * $var2;
+}
+echo someMath(10, 5);
+
+
+$names = array("Fred", "Barney", "Wilma", "Betty");
+function &findOne($n) {
+    global $names;
+    return $names[$n];
+}
+$person = & findOne(1); // Barney
+$person = "Barnetta"; // changes $names[1]
+
+$array = array("really long string here, boy", "this", "middling length",
+    "larger");
+usort($array, function($a, $b) {
+    return strlen($a)-strlen($b);
+});
+print_r($array);
+
+
+$array = array("really long string here, boy", "this", "middling length",
+    "larger");
+$sortOption = 'random';
+usort($array, function($a, $b) use ($sortOption)
+{
+    if ($sortOption == 'random') {
+// sort randomly by returning (-1, 0, 1) at random
+        return rand(0, 2) - 1;
+    }
+    else {
+        return strlen($a) - strlen($b);
+    }
+});
+
+print_r($array);
+
 
 $last=1;
